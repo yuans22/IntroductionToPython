428a079453f57cb9cdea12246650c1493b7b6201
yuans <yuans@rose-hulman.edu>
2017-11-27 14:40:22 -0500

    DONE

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -76,7 +76,7 @@ print("Hi")
 print(3607*34227)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in this project to the left.
 #   Note that this file (m2_hello_world.py) is now displayed in a blue
 #   font color (if the file is highlighted select a different file so yu can
