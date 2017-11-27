67b5c65bf2e7afd719274dab34dbab5965cec3ea
yuans <yuans@rose-hulman.edu>
2017-11-27 16:52:37 -0500

    DONE

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Shuai Yuan.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,45 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+zhang = rg.SimpleTurtle('turtle')
+zhang.pen = rg.Pen('blue', 5)
+zhang.speed = 10
+radius = 150
+for k in range(6):
+    zhang.draw_circle(radius)
+    zhang.pen_up()
+    zhang.right(45)
+    zhang.forward(10)
+    zhang.left(45)
+    zhang.pen_down()
+    radius = radius - 12
+
+zheng = rg.SimpleTurtle('turtle')
+zheng.pen = rg.Pen ('red', 5)
+zheng.speed = 10
+size = 100
+for k in range (6):
+    zheng.draw_regular_polygon(10,size)
+    zheng.pen_up()
+    zheng.right(45)
+    zheng.forward(10)
+    zheng.left(45)
+    zheng.pen_down()
+    size = size - 12
+
+zz = rg.SimpleTurtle('turtle')
+zz.pen = rg.Pen('green', 5)
+zz.speen = 10
+side = 100
+for k in range (6):
+    zz.draw_square(side)
+    zz.pen_up()
+    zz.right(45)
+    zz.forward(10)
+    zz.left(45)
+    zz.pen_down()
+    side = side -12
