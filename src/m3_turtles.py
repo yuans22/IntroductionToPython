9b03b99215fcee7ffca05a878e40dd6d394c9b10
yuans <yuans@rose-hulman.edu>
2017-11-27 15:00:43 -0500

    DONE

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -75,9 +75,10 @@ matt.left(90)
 matt.forward(50)
 
 
+
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -88,10 +89,13 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+matt.backward(100)
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +113,22 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+RT = rg.SimpleTurtle('turtle')
+RT.pen = rg.Pen('green',25)
+RT.speed = 8
+RT.left(45)
+RT.forward(50)
+RT.right(45)
+RT.forward(50)
+RT.right(45)
+RT.forward(50)
+RT.right(90)
+RT.forward(180)
+RT.left(135)
+RT.forward(130)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
